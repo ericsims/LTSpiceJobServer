4b280412c8417d583b0bad8e836a39283e3277ec
Eric Sims <EricSimsM@gmail.com>
2020-06-07 20:26:01 -0600

    more eff. way to do it. Creates only 1 file per worker instead of 1 file per run.

--- parse.py
+++ parse.py
@@ -3,6 +3,10 @@ import re
 from queue import Queue
 from threading import Thread
 from time import time
+import itertools
+import math
+import string
+import random
 
 LTSpicePath = 'C:\Program Files\LTC\LTspiceXVII\XVIIx64.exe'
 ##file = 'example LTSpice sims/BuckBoost'
@@ -13,8 +17,10 @@ returned_value = subprocess.run([LTSpicePath, '-netlist', file+'.asc'], shell=Tr
 print('returned value:', returned_value)
 
 runslist=[]
-
 lines = []
+
+stepvals = []
+stepvars = []
 
 class LTSpiceWorker(Thread):
 
@@ -78,12 +84,15 @@ def parseStepDirec(stepdirective):
 
 ##    print("mode: {}, var: {}, params:{}".format(mode,var,params))
 ##    print("values:{}".format(values))
+    stepvars.append(var)
+    stepvals.append(tuple(values[:]))
+    return ''
 
-    paramStrList = []
-    for value in values:
-        paramStrList.append('.param {} {}'.format(var, value))
+##    paramStrList = []
+##    for value in values:
+##        paramStrList.append('.param {} {}'.format(var, value))
 ##        print('.param {} {}'.format(var, value))
-    return paramStrList
+##    return paramStrList
 
 
 specialdirectives=[\
@@ -123,8 +132,8 @@ with open(file+".net", 'r') as fp:
                 else:
                     line=line.strip()
                 if not direc['function'] is None:
-                    line = parseStepDirec(line)
-                lines.append(line)
+                    line = direc['function'](line)
+                lines.append(line.strip())
                 print("Line {:>4}:  {:<8}  {}".format(cnt, direc['str'], line))
                     
         if not isaspecialdirec:
@@ -135,19 +144,71 @@ with open(file+".net", 'r') as fp:
         cnt += 1
 
     print()
-    output = []
-    output.append([])
-    evalLines(output, output[0], 0, len(lines))
-    for i in range(0, len(output)):
-        out = open('j{}.net'.format(i),'w')
-        for line in output[i]:
-            out.write('{}\r\n'.format(line))
-        runslist.append(str(out.name))
+    
+##    output = []
+##    output.append([])
+##    evalLines(output, output[0], 0, len(lines))
+##    for i in range(0, len(output)):
+##        out = open('j{}.net'.format(i),'w')
+##        for line in output[i]:
+##            out.write('{}\r\n'.format(line))
+##        runslist.append(str(out.name))
+##        out.close()
+
+def execRuns(workerCount_):
+    runList_ = []
+    combinations = [list(sub) for sub in list(itertools.product(*stepvals))[:]]
+    numberofsteps = len(combinations)
+    if numberofsteps > 1:
+        lookuptable = []
+        j = 0
+        s = 0
+        for vi in range(numberofsteps):
+    ##        print('j{}, s{}, vi{}, v{}'.format(j,s,vi,combinations[vi]))
+            if not (len(lookuptable) > j):
+                lookuptable.append([])
+            lookuptable[j].append(combinations[vi])
+            j += 1
+            if j >= workerCount_:
+                j = 0
+                s += 1
+        for j in range(len(lookuptable)):
+            out = open('j{}.net'.format(j),'w')
+            runList_.append(str(out.name))
+            newlines = lines[:]
+##            print()
+##            print('j{}'.format(j))
+            dummylength = len(lookuptable[j])
+            if dummylength > 1:
+        ##        dummyvar = ''.join(random.choice(string.ascii_lowercase) for i in range(16))
+                dummyvar = 'temporarystepvarx'
+##                print('.step param {} 0 {} 1'.format(dummyvar,dummylength-1))
+                newlines.insert(len(newlines)-2,'.step param {} 0 {} 1'.format(dummyvar,dummylength-1))
+                for x in range(len(stepvars)):
+                    cmd = '.param {} table({}'.format(stepvars[x],dummyvar)
+                    for y in range(len(lookuptable[j])):
+                        cmd+=(',{},{}'.format(y,lookuptable[j][y][x]))
+                    cmd+=(')')
+                    newlines.insert(len(newlines)-2,cmd)
+##                    print(cmd)
+            else:
+                for x in range(len(stepvars)):
+                    cmd = '.param {} {}'.format(stepvars[x],lookuptable[j][0][x])
+                    newlines.insert(len(newlines)-2,cmd)
+##                    print(cmd)
+            for line in newlines:
+                out.write('{}\n'.format(line))
+            out.close()
+    else:
+        out = open('j0.net','w')
+        for line in lines:
+           out.write('{}\n'.format(line))
         out.close()
 
-def execRuns(runList_, workerCount_):
+
+    
     print()
-    print('doing {} runs with {} workers'.format(len(runList_), workerCount_))
+    print('doing {} steps in {} runs with {} workers'.format(len(combinations), len(runList_), workerCount_))
     start = time()
     queue = Queue()
     for x in range(workerCount_):
@@ -162,11 +223,12 @@ def execRuns(runList_, workerCount_):
     elapsed = end - start
     print('elapsed: ', elapsed)
 
-workerCounts = [1,2,4,6,8,10,12,14,16,20,24,28,32]
-##workerCounts = [40,48,64,128]
+workerCounts = [1,2,4,8,12,16,24,32,48,64,128]
+##workerCounts = [16]
 
 for x in workerCounts:
-    execRuns(runslist, x)
+    execRuns(x)
+    
 ##for job in runslist:
 ##    returned_value = subprocess.run([LTSpicePath, '-b', job], shell=True, check=True)
 ##    print('job:{} returned:{}'.format(job,returned_value))
